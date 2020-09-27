2fc4e7e83b369649b534f0676e8eb1741442cffa
Eytan Manor <emanor6@gmail.com>
2020-09-27 20:44:40 +0300

    Restore missing hamburger example

--- examples/hamburger/src/App.js
+++ examples/hamburger/src/App.js
@@ -1,66 +1,109 @@
 import './App.css';
 
 import React, { useCallback, useState } from 'react';
-import { createLayer, usePushLayer, Stack } from 'react-layers-stack';
+import { Stack, createLayer } from 'react-layers-stack';
+
+import BottomBunLayer from './components/BottomBunLayer';
+import CheeseLayer from './components/CheeseLayer';
+import LettuceLayer from './components/LettuceLayer';
+import OnionLayer from './components/OnionLayer';
+import PattyLayer from './components/PattyLayer';
+import PickleLayer from './components/PickleLayer';
+import TomatoLayer from './components/TomatoLayer';
+import TopBunLayer from './components/TopBunLayer';
+import DoneLayer from './components/DoneLayer';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <TabsMenu />
-    </div>
-  );
-};
-
-const renderTab = ({ layerIndex }) => (
-  <div style={{ background: 'pink', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-    Page {layerIndex}
-  </div>
-);
-
-const TabsMenu = () => {
   const [layers, setLayers] = useState([
-    createLayer(renderTab({ layerIndex: 1 })),
+    createLayer(
+      <BottomBunLayer />
+    ),
   ]);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <div style={{ flex: .15, background: 'lightblue', padding: 20 }}>header</div>
-      <div style={{ flex: .85, overflow: 'hidden' }}>
-        <Stack layersState={[layers, setLayers]}>
-          {stack => {
-            const [layerIndex, setLayerIndex] = useState(1);
-            const pushLayer = usePushLayer();
+    <div className='App'>
+      <Stack layersState={[layers, setLayers]}>
+        {stack => {
+          const [layerIndex, setlayerIndex] = useState(1);
+          const pushPattyLayer = PattyLayer.usePush();
+          const pushCheeseLayer = CheeseLayer.usePush();
+          const pushTomatoLayer = TomatoLayer.usePush();
+          const pushPickleLayer = PickleLayer.usePush();
+          const pushOnionLayer = OnionLayer.usePush();
+          const pushLettuceLayer = LettuceLayer.usePush();
+          const pushTopBunLayer = TopBunLayer.usePush();
+          const pushDoneLayer = DoneLayer.usePush();
 
-            const handleClick = useCallback(() => {
-              switch (layerIndex) {
+          const handleClick = useCallback(() => {
+            switch (layerIndex) {
+              case 1:
+                setlayerIndex(2);
+
+                return pushPattyLayer();
+
+              case 2:
+                setlayerIndex(3);
+
+                return pushCheeseLayer();
+
               case 3:
+                setlayerIndex(4);
 
-              default:
-                setLayerIndex(i => ++i);
+                return pushTomatoLayer();
 
-                return pushLayer(renderTab({ layerIndex: layerIndex + 1 }), {
-                  keyframes: [
-                    { width: '100%', left: 'auto', right: '-100%' },
-                    { width: '100%', left: 'auto', right: '0' },
-                  ],
-                  timing: {
-                    duration: 500,
-                  },
-                  mask: {
-                    background: '#000',
-                  },
-                });
-              }
-            }, [layerIndex, pushLayer]);
+              case 4:
+                setlayerIndex(5);
 
-            return (
-              <span onClick={handleClick}>
-                {stack}
-              </span>
-            );
-          }}
-        </Stack>
-      </div>
+                return pushPickleLayer();
+
+              case 5:
+                setlayerIndex(6);
+
+                return pushOnionLayer();
+
+              case 6:
+                setlayerIndex(7);
+
+                return pushLettuceLayer();
+
+              case 7:
+                setlayerIndex(8);
+
+                return pushTopBunLayer();
+
+              case 8:
+                setlayerIndex(9);
+
+                return pushDoneLayer();
+
+              case 9:
+                setlayerIndex(1);
+
+                return setLayers([
+                  createLayer(
+                    <BottomBunLayer />
+                  )
+                ]);
+            }
+          }, [
+            layerIndex,
+            pushPattyLayer,
+            pushCheeseLayer,
+            pushTomatoLayer,
+            pushPickleLayer,
+            pushOnionLayer,
+            pushLettuceLayer,
+            pushTopBunLayer,
+            pushDoneLayer,
+          ]);
+
+          return (
+            <div className='App-stack' onClick={handleClick}>
+              {stack}
+            </div>
+          );
+        }}
+      </Stack>
     </div>
   );
 };
